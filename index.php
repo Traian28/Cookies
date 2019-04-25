c079da16137437c0079fd725d3e85cce161867f9
Traian Alimenti - Freelancer <traian.alimentibel@gmail.com>
2019-04-25 16:54:00 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+setcookie('font-size','30px',time() + 60 * 60 * 24 * 30,'/');
+
+?>
+
+</!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+</head>
+<body>
+	<h1>Cookie Seteada</h1>
+
+</body>
+</html>
